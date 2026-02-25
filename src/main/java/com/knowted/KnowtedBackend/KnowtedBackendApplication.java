6e323add10550d1aeed7c60dc260ba4c3435bbc2
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2026-02-25 18:40:54 -0500

    initialized the repo

---
+++ src/main/java/com/knowted/KnowtedBackend/KnowtedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.knowted.KnowtedBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowtedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowtedBackendApplication.class, args);
+	}
+
+}
