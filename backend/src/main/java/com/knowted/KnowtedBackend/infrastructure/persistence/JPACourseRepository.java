9921c976e5856d22ef590345ddccb71a871d10c9
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2026-03-07 15:28:33 -0500

    working on concrete infra classes

--- backend/src/main/java/com/knowted/KnowtedBackend/infrastructure/persistence/JPACourseRepository.java
+++ backend/src/main/java/com/knowted/KnowtedBackend/infrastructure/persistence/JPACourseRepository.java
@@ -1,4 +1,20 @@
 package com.knowted.KnowtedBackend.infrastructure.persistence;
 
-public class JPACourseRepository {
+import com.knowted.KnowtedBackend.domain.entity.Course;
+import com.knowted.KnowtedBackend.domain.entity.CourseDocument;
+import com.knowted.KnowtedBackend.domain.repository.CourseRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public class JPACourseRepository implements CourseRepository {
+
+    public List<CourseDocument> findAll(){
+
+    }
+    public Course findById(UUID id){}
+    public Course create(Course course){}
+    public void update(UUID courseId, Course course){}
+    public void deleteById(UUID id){}
+
 }
