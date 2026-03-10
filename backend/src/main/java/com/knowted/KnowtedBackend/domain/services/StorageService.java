8d69260ef3f65b4a264bc10cef8f04ed97a3eef4
Aaron Kofi Haizel <kofi12@my.yorku.ca>
2026-03-10 13:35:55 -0400

    GCSStorageService completed except for exists method

--- backend/src/main/java/com/knowted/KnowtedBackend/domain/services/StorageService.java
+++ backend/src/main/java/com/knowted/KnowtedBackend/domain/services/StorageService.java
@@ -5,7 +5,7 @@ import java.time.Duration;
 
 public interface StorageService {
 
-    public String upload(InputStream contentStream, String fileName, String contentType, long contentLength);
+    public String upload(InputStream contentStream, String fileName, String contentType);
     public String getPresignedDownloadUrl(String storageKey, Duration expiration);
     void delete(String storageKey);
     boolean exists(String storageKey);
--- backend/src/main/java/com/knowted/KnowtedBackend/infrastructure/gcs/GCSStorageService.java
+++ backend/src/main/java/com/knowted/KnowtedBackend/infrastructure/gcs/GCSStorageService.java
@@ -4,8 +4,13 @@ import com.google.cloud.storage.*;
 import com.knowted.KnowtedBackend.domain.services.StorageService;
 import org.springframework.beans.factory.annotation.Value;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.time.Duration;
+import java.time.Instant;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 public class GCSStorageService implements StorageService {
 
@@ -18,18 +23,39 @@ public class GCSStorageService implements StorageService {
     }
 
     @Override
-    public String upload(InputStream contentStream, String fileName, String contentType, long contentLength) {
-        return "";
+    public String upload(InputStream contentStream, String fileName, String contentType) {
+
+        String storageKey = "documents/" + Instant.now().toEpochMilli() + "_" + UUID.randomUUID().toString().substring(0, 6) +
+                fileName;
+
+        BlobId blobId = BlobId.of(bucketName, storageKey);
+        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType(contentType).build();
+
+        try {
+            googleStorage.createFrom(blobInfo, contentStream);
+        } catch(IOException e) {
+            throw new RuntimeException("Failed to upload content to GCS", e);
+        }
+
+        return storageKey;
     }
 
     @Override
     public String getPresignedDownloadUrl(String storageKey, Duration expiration) {
-        return "";
+
+        BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, storageKey).build();
+
+        URL signedUrl = googleStorage.signUrl(blobInfo, expiration.getSeconds(), TimeUnit.SECONDS,
+                Storage.SignUrlOption.httpMethod(HttpMethod.GET), Storage.SignUrlOption.withV4Signature());
+
+        return signedUrl.toString();
     }
 
     @Override
     public void delete(String storageKey) {
 
+        BlobId blobId = BlobId.of(bucketName, storageKey);
+        googleStorage.delete(blobId);
     }
 
     @Override
